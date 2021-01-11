50d21d71b2f478a83ce481306a6b9fe1cfe5c873
Allyson <allysoneduardo04@gmail.com>
2021-01-11 19:47:55 -0300

    feat: adding onfocus change of background on input

---
+++ script.js
@@ -0,0 +1,11 @@
+// First we create the function responsible for making the input background turn yellow
+
+function focusButton () {
+    document.getElementById("password_input").className = "backgroundYellow"
+}
+
+//then we create restorePassword, to get the value on the input and define if the background turns red or green
+
+function restorePassword () {
+
+}
